e627973f2a7fb579e71d3528728cf72d623941cf
Soumak Chakraborty <chakrabortysoumak11@gmail.com>
2019-04-01 14:31:42 +0530

    Mid commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express=require("express");
 var app=express();
+var f=require("fs-extra");
 var conn=require("./modules/connection");
 var encrypt=require("./modules/Encrypt");
 var session=require("express-session");
@@ -7,6 +8,7 @@ var sanitizer=require("express-sanitizer");
 var body=require("body-parser");
 var files=require("express-fileupload");
 var method=require("method-override");
+var email=require("emailjs");
 app.use(body.urlencoded({extended:true}));
 var c=0;
 var uname="";
@@ -133,10 +135,10 @@ app.get("/showuser",function(req,res)
              conn.query("select * from album where user='"+results[0].username+"'",function(e,r,f)
              {
                   if(r.length==0)
-                    res.render("showuser",{username:uname,album:""});
+                    res.render("showuser",{username:uname,album:"",usermail:results[0].username});
                   else
                   {
-                    res.render("showuser",{username:uname,album:r});
+                    res.render("showuser",{username:uname,album:r,usermail:results[0].username});
                   }
              });
          });
@@ -158,6 +160,14 @@ app.get("/create/album",function(req,res)
 });
 app.post("/create/album",function(req,res)
 {
+  var IP=req.connection.remoteAddress;
+  var user=req.headers["user-agent"];
+  var sessid=encrypt(IP+""+user);
+  conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+  {
+      if(result.length==0)
+        res.redirect("/check");
+  });
    var album_name=req.sanitize(req.body.albumname);
    var description=req.sanitize(req.body.albumdescription);
    var date=new Date();
@@ -180,6 +190,14 @@ app.post("/create/album",function(req,res)
 });
 app.get("/show/album/:userid/:id",function(req,res)
 {
+  var IP=req.connection.remoteAddress;
+   var user=req.headers["user-agent"];
+   var sessid=encrypt(IP+""+user);
+  conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+  {
+      if(result.length==0)
+        res.redirect("/check");
+  });
     conn.query("select * from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(err,result,fields)
     {
          conn.query("select * from album where user='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(e,r,f)
@@ -190,6 +208,14 @@ app.get("/show/album/:userid/:id",function(req,res)
 });
 app.get("/create/photo/:userid/:id",function(req,res)
 {
+  var IP=req.connection.remoteAddress;
+   var user=req.headers["user-agent"];
+   var sessid=encrypt(IP+""+user);
+  conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+  {
+      if(result.length==0)
+        res.redirect("/check");
+  });
     res.render("createphoto",{user:req.params.userid,album:req.params.id});
 });
 app.post("/create/photo/:userid/:id",function(req,res)
@@ -211,6 +237,14 @@ app.post("/create/photo/:userid/:id",function(req,res)
 });
 app.get("/Edit/:userid/:id",function(req,res)
 {
+  var IP=req.connection.remoteAddress;
+   var user=req.headers["user-agent"];
+   var sessid=encrypt(IP+""+user);
+  conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+  {
+      if(result.length==0)
+        res.redirect("/check");
+  });
      conn.query("select * from album where user='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(err,result,fields)
      {
          res.render("editalbum",{result:result});
@@ -241,7 +275,150 @@ app.put("/Edit/:userid/:id",function(req,res)
     });  
     res.redirect("/show/album/"+req.params.userid+"/"+alb); 
 });
-app.listen(3000,"127.0.0.1",function()
+app.delete("/Delete/:userid/:id",function(req,res)
+{
+    conn.query("select photo_name from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(error,result,fields)
+    {
+          for(var i=0;i<result.length;i++)
+          {
+            f.unlink("./Public/uploads/"+result[i],function(err)
+            {
+                 if(err)
+                   console.log(err);
+            });
+          }
+    });
+    conn.query("delete from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(error,result,fields)
+    {
+    });
+    conn.query("delete from album where user='"+req.params.userid+"'"+"and album_name='"+req.params.id+"'",function(error,result,fields)
+    {
+    }); 
+   res.redirect("/showuser"); 
+});
+app.get("/show/:userid/:album/:id",function(req,res)
+{
+  var IP=req.connection.remoteAddress;
+  var user=req.headers["user-agent"];
+  var sessid=encrypt(IP+""+user);
+ conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+ {
+     if(result.length==0)
+       res.redirect("/check");
+  });
+    conn.query("select * from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.album+"'"+"and photo_name='"+req.params.id+"'",function(err,result,fields)
+    {
+      res.render("showimage",{result:result,username:req.params.userid,album:req.params.album});
+    });
+});
+app.get("/Edit/:userid/:album/:id",function(req,res)
+{
+  var IP=req.connection.remoteAddress;
+   var user=req.headers["user-agent"];
+   var sessid=encrypt(IP+""+user);
+  conn.query("select * from state where IP='"+IP+"'"+"and sessid='"+sessid+"'",function(err,result,fields)
+  {
+      if(result.length==0)
+        res.redirect("/check");
+  });
+  conn.query("select * from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.album+"'"+"and photo_name='"+req.params.id+"'",function(err,result,fields)
+  {
+    res.render("editimage",{result:result,username:req.params.userid,album:req.params.album});
+  });
+});
+app.put("/Edit/:userid/:album/:id",function(req,res)
+{
+    var photoname=req.body.photoname;
+    var description=req.body.photodescription;
+    if(req.params.id!=photoname)
+    {
+      f.rename("./Public/Uploads/"+req.params.id,"./Public/Uploads/"+photoname+"_"+req.params.userid+".jpg",function(err)
+      {
+         if(err)
+           console.log(err);
+      });
+    }
+   conn.query("update photos set photo_name='"+photoname+"_"+req.params.userid+".jpg"+"'"+"where username='"+req.params.userid+"'"+"and album_name='"+req.params.album+"'",function(error,result,fields)
+   {
+   });
+   conn.query("update photos set description='"+description+"'"+"where username='"+req.params.userid+"'"+"and album_name='"+req.params.album+"'",function(error,result,fields)
+   {
+   });
+   if(req.params.id!=photoname) 
+     res.redirect("/show/"+req.params.userid+"/"+req.params.album+"/"+photoname+"_"+req.params.userid+".jpg");
+   else
+   res.redirect("/show/"+req.params.userid+"/"+req.params.album+"/"+req.params.id);
+});
+app.delete("/Delete/:userid/:album/:id",function(req,res)
+{
+         f.unlink("./Public/uploads/"+req.params.id,function(error)
+         {
+            if(error)
+              console.log(error);      
+         });
+        conn.query("delete from photos where username='"+req.params.userid+"'"+"and album_name='"+req.params.album+"'"+"and photo_name='"+req.params.id+"'",function(err,result,fields)
+        {
+        });
+    res.redirect("/show/album/"+req.params.userid+"/"+req.params.album);    
+});
+app.get("/changepassword/:id",function(req,res)
+{
+   res.render("changepass",{username:req.params.id});
+});
+app.put("/changepassword/:id",function(req,res)
+{
+   var password=req.body.password;
+   var firstpass=encrypt(password);
+   firstpass="P"+firstpass+"D";
+   var secondpass=encrypt(firstpass);
+   conn.query("update user set password='"+secondpass+"'"+"where username='"+req.params.id+"'",function(err,result,fields)
+   {
+       if(err)
+         console.log(err);
+   });
+   res.redirect("/showuser");
+});
+app.get("/forgotpass",function(req,res)
+{
+   res.render("forgotpass");
+});
+app.post("/forgotpass",function(req,res)
+{
+    var usermail=req.body.username;
+    var server=email.server.connect({
+        user:"yelpcamp500@gmail.com",
+        password:"Windows90#",
+        host:"smtp.gmail.com",
+        ssl:true
+    });
+    server.send({
+        from:"yelpcamp500@gmail.com",
+        to:usermail,
+        text:"Please click on the link: http://localhost:3000/resetpass/"+usermail+" \n to reset your password\nRegards,\nDigital Frame team"
+    },function(err,msg)
+    {
+        console.log(err||msg);
+    });
+    res.redirect("/check");
+});
+app.get("/resetpass/:id",function(req,res)
+{
+    res.render("resetpassword",{username:req.params.id});
+});
+app.put("/resetpass/:id",function(req,res)
+{
+  var password=req.body.password;
+  var firstpass=encrypt(password);
+  firstpass="P"+firstpass+"D";
+  var secondpass=encrypt(firstpass);
+  conn.query("update user set password='"+secondpass+"'"+"where username='"+req.params.id+"'",function(err,result,fields)
+  {
+      if(err)
+        console.log(err);
+  });
+  res.redirect("/check");
+});
+app.listen(3000,"192.168.0.101",function()
 {
      console.log("Server has started");
 });
